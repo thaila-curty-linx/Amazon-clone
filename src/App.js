de2e9f85e47706051f3d3e33f5d7ccf5c86887b2
thailajf <thailajf@hotmail.com>
2020-12-09 05:55:01 -0300

    home page - doing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
+import React from 'react';
+import Header from './Components/Header/index.js';
+import Home from './Pages/Home/index';
 
-import React from 'react'
-import Header from './Components/Header/index.js'
 function App() {
   return (
     <div className="App">
-      <Header/>
-      {/* Home */}
-
-          </div>
+      <Header />
+      <Home />
+    </div>
   );
 }
 
---
+++ src/Pages/Home/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles.css';
+function index() {
+  return (
+    <div className="home">
+      <div className="container">
+        <img
+          className="image"
+          src="https://images-na.ssl-images-amazon.com/images/G/32/prime/Merchandising/2020_Graphics/Shipping/XCM_Manual_ORIGIN_1249933_1307106_BR_BR_DesktopHero_copy_1_3308715_1500x600_1X_pt_BR._CB406090573_.jpg"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default index;
